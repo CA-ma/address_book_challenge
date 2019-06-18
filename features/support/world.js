bd5d97c74e2f4aebd8573f152617bb8f654c718d
Max Aubain <max.aubain@gmail.com>
2019-06-18 17:23:29 +0200

    Add function and test for Add Contacts button click

--- features/support/world.js
+++ features/support/world.js
@@ -5,24 +5,33 @@ const puppeteer = require('puppeteer')
 const HOME_PAGE = 'http://localhost:3000'
 
 class AddressBookWorld {
-  constructor() {}
+    constructor() {}
 
-  // Open the home page using puppeteer
-  async openHomePage() {
-    this.browser = await puppeteer.launch({headless: false, slowmo: 100})
-    this.page = await this.browser.newPage()
-    await this.page.goto(HOME_PAGE)
-  }
-  // Close the home page using puppeteer
+    // Open the home page using puppeteer
+     async openHomePage() {
+       this.browser = await puppeteer.launch({headless: false, slowmo: 100})
+       this.page = await this.browser.newPage()
+      await this.page.goto(HOME_PAGE)
+    }
+
+    // Close the home page using puppeteer
     async closeHomePage() {
-    await this.browser.close()
-  }
+        await this.browser.close()
+    }
+
     async pageHasTextContent(expectedContent) {
         const pageContent = await this.page.content()
         const actualContent = pageContent.match(expectedContent)[0]
 
         expect(actualContent).to.be.eq(expectedContent)
     }
+
+    async clickOnAddContactBtn() {
+        const btnSelector = '.add-contact'
+        await this.page.waitForSelector(btnSelector)
+        await this.page.click(btnSelector)
+    }
+
 }
 
 setWorldConstructor(AddressBookWorld)
