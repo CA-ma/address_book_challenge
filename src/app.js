85d0df23092b496c057e548349c2d421488a7309
Max Aubain <max.aubain@gmail.com>
2019-06-20 17:24:06 +0200

    Added button to reset contact list by clearing local storage

--- src/app.js
+++ src/app.js
@@ -1,63 +1,74 @@
+// Establish local storage variable
 const storage = window.localStorage;
 
-document.getElementById('hide').style.display= "none"
+// Function to reveal and hide new contact form
+document.getElementById('hide').style.display = "none"
 
 let a = 1
 
-function showForm () {
+function showForm() {
     a += 1
-    if(a % 2 == 0){
-        document.getElementById('hide').style.display= "block";
+    if (a % 2 == 0) {
+        document.getElementById('hide').style.display = "block";
     }
     else {
-        document.getElementById('hide').style.display= "none";
+        document.getElementById('hide').style.display = "none";
     }
 }
 
 document.getElementById("add-contact").addEventListener("click", showForm)
 document.getElementById("save-contact").addEventListener("click", showForm)
 
+// Function to clear local storage of contact information
+document.getElementById("reset-contacts").addEventListener("click", memWipe)
 
+function memWipe() {
+    window.localStorage.clear()
+    renderContacts()
+}
+
+
+// Function to show contacts currently in 
 const renderContacts = () => {
     const contacts = JSON.parse(storage.getItem('contacts'))
-    
+
     let div = document.querySelector('.contact-list');
     div.innderHTML = ''
-    if (contacts) {        
+    if (contacts) {
         const ul = document.createElement('ul')
         ul.className = "list-reset";
-        
+
         contacts.forEach(contact => {
             let li = document.createElement('li')
             li.innerHTML = `
                 <div class="card">
                     <div class="content">
-                        <h1>${ contact.name }</h1>
-                        <h2>${ contact.company }</h2>
-                        <p>${ contact.notes }</p>
-                        ${ contact.email } |
+                        <h1>${ contact.name}</h1>
+                        <h2>${ contact.company}</h2>
+                        <p>${ contact.notes}</p>
+                        ${ contact.email} |
                         <a href="https://www.twitter.com/${ contact.twitter}">@${contact.twitter}</a>
                     </div>
                 </div>
             `
             ul.appendChild(li)
         })
-        
+
         div.appendChild(ul)
     } else {
         div.innerHTML = `<p>You have no contacts in your address book</p>`
     }
 }
 
-document.addEventListener('DOMContentLoaded', () => {    
+document.addEventListener('DOMContentLoaded', () => {
     renderContacts()
     const addContactForm = document.querySelector('#new-contact-form')
     addContactForm.addEventListener('submit', event => {
         let clearMessage = document.querySelector('.contact-list');
         clearMessage.innerHTML = ""
-        event.preventDefault()        
-        const storage = window.localStorage;    
-        const{
+        event.preventDefault()
+        const storage = window.localStorage;
+        const {
             name,
             email,
             phone,
@@ -66,7 +77,7 @@ document.addEventListener('DOMContentLoaded', () => {
             twitter,
         } = addContactForm.elements
 
-        const contact ={
+        const contact = {
             id: Date.now(),
             name: name.value,
             email: email.value,
@@ -76,7 +87,7 @@ document.addEventListener('DOMContentLoaded', () => {
             twitter: twitter.value,
         }
         console.log(`Saving the following contact: ${JSON.stringify(contact)}`);
-        
+
         let contacts = JSON.parse(storage.getItem('contacts')) || []
         contacts.push(contact)
         storage.setItem('contacts', JSON.stringify(contacts))
