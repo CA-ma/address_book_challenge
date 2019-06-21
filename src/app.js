a54e527f491210aa0518a17258c12131b927f4e1
Max Aubain <max.aubain@gmail.com>
2019-06-21 11:59:59 +0200

    Delete contact is functional, deleting top entry

--- src/app.js
+++ src/app.js
@@ -3,9 +3,7 @@ const storage = window.localStorage;
 
 // Function to reveal and hide new contact form
 document.getElementById('hide').style.display = "none"
-
 let a = 1
-
 function showForm() {
     a += 1
     if (a % 2 == 0) {
@@ -15,7 +13,6 @@ function showForm() {
         document.getElementById('hide').style.display = "none";
     }
 }
-
 document.getElementById("add-contact").addEventListener("click", showForm)
 document.getElementById("save-contact").addEventListener("click", showForm)
 
@@ -27,29 +24,45 @@ function memWipe() {
     renderContacts()
 }
 
-// Function to clear an item(s) from local storage
+// Function to clear top item from local storage
 document.getElementById("delete-contact").addEventListener("click", memDeleteItem)
 
 function memDeleteItem() {
     const contacts = JSON.parse(localStorage.getItem('contacts'));
-    console.log("contacts array", contacts)
-    console.log("first entry in contacts array", contacts[0])
-    console.log("length of contacts array", contacts.length)
-    console.log("return id value of first entry in contacts", contacts[0].id)
+    //console.log("contacts array", contacts)
+    //console.log("first entry in contacts array", contacts[0])
+    //console.log("length of contacts array", contacts.length)
+    //console.log("return id value of first entry in contacts", contacts[0].id)
 
-    // let i = localStorage.length;
-    // while (i-- > 0) {
-    //     let key = localStorage.key(i);
-    //     if (localStorage.getItem(key) === 'Max Aubain') {
-    //         localStorage.removeItem(key);
-    //     }
-    // }
+    // Delete 1 contacts array item at index "index" and re-write to memory
+    contacts.splice(0, 1)
+    console.log("contact array with [0] item removed", contacts)
 
-    //renderContacts()
+    // Send edited contacts list back to localStorage
+    storage.setItem('contacts', JSON.stringify(contacts))
+
+    // Clear current rendering of contact-list div
+    let clearMessage = document.querySelector('.contact-list');
+    clearMessage.innerHTML = ""
+
+    // Render edited contacts list
+    renderContacts()
 }
 
+// Function to assign event listeners to newly created "delete" buttons
 
-// Function to show contacts currently in 
+// let update = false
+// if (update == true){
+//     for (let i = 0; i < 100; i++){
+//         let label = "delete-" + String(i) 
+//         console.log(label)
+//         document.getElementById(label).addEventListener("click", memDeleteItem(i))
+//     }
+//     update = false
+// }
+
+
+// Function to show contacts currently in local storage
 const renderContacts = () => {
     const contacts = JSON.parse(storage.getItem('contacts'))
 
@@ -59,6 +72,7 @@ const renderContacts = () => {
         const ul = document.createElement('ul')
         ul.className = "list-reset";
 
+        let index = 0
         contacts.forEach(contact => {
             let li = document.createElement('li')
             li.innerHTML = `
@@ -69,19 +83,21 @@ const renderContacts = () => {
                         <p>${ contact.notes}</p>
                         ${ contact.email} |
                         <a href="https://www.twitter.com/${ contact.twitter}">@${contact.twitter}</a>
+                        <button class="ui inverted red button" id="delete-${index}">Delete</button>
                     </div>
                 </div>
             `
             ul.appendChild(li)
+            index += 1
         })
-
         div.appendChild(ul)
-    } else {
+    } 
+    else {
         div.innerHTML = `<p>You have no contacts in your address book</p>`
     }
 }
 
-// Process to show contacts list, persistent between sessions as contacts are stored in local storage
+// Process to show contacts list stored in memory, persistent between sessions as contacts are stored in localStorage memory
 document.addEventListener('DOMContentLoaded', () => {
     renderContacts()
     const addContactForm = document.querySelector('#new-contact-form')
